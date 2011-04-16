676d1c54ff13b94ebc14a35a280959410dff83d9
Akash Manohar J <akash@akash.im>
2011-04-16 18:37:55 +0530

    added 2.4.2
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ 2/2.4.2.scm
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.4.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define (f x)
+  (+ x 10))
+
+(define (g x)
+  (+ x 10))
+
+(define (h x)
+  (+ x 10))
